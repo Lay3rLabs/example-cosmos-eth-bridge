9c653442c05b6e9be8775dd1b8944eab9423808f
David Komer <6406986+dakom@users.noreply.github.com>
2025-02-10 18:33:25 +0200

    rename

--- handler-deploy/DeployHandler.s.sol
+++ handler-deploy/DeployHandler.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
 import "contracts/solidity/Submit.sol";
-import {ILayerServiceManager} from "@layer/interfaces/ILayerServiceManager.sol";
+import {ILayerServiceManager} from "@wavs/interfaces/ILayerServiceManager.sol";
 
 contract Deploy is Script {
     function run(string calldata mnemonic, string calldata serviceManager) external {
